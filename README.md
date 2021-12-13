d562ed71782366b94ca0d8f680fc43eee30d16ba
Pol <playolaizq@gmail.com>
2021-12-13 09:52:53 +0100

    reproduce bug

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ npm start
 ```
 
 See http://localhost:3000.
+
+## How to reproduce it
+
+You will see that (optional) label for the Form.Item when locale is set as
+"ca_ES" is translated to English because "Form.optional" translation key is
+missing for Catalan.
+
+If you set the locale as "es_ES" you will see that it translate it properly.
+
+## Expected translations
+
+Default: "(optional)"
+Spanish (es_ES): "(opcional)"
+Catalan (ca_ES): "(opcional)" <-- missing
--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-function App() {
+import { useState } from 'react';
+import { ConfigProvider, Form, Input, Button } from 'antd';
+import esES from 'antd/lib/locale/es_ES';
+import caES from 'antd/lib/locale/ca_ES';
+import "antd/dist/antd.css";
+
+const locales = {
+  ES: esES,
+  CA: caES,
+};
+
+const App = () => {
+  const [form] = Form.useForm();
+
+  const [locale, setLocale] = useState(locales.CA);
+
   return (
-    <div>
-      Ant Design
-    </div>
+    <ConfigProvider locale={locale}>
+      <Form form={form} requiredMark="optional" style={{ padding: 64 }}>
+        <Form.Item label="Field A" required>
+          <Input placeholder="Required field" />
+        </Form.Item>
+        <Form.Item label="Field B">
+          <Input placeholder="Optional field" />
+        </Form.Item>
+        <Button type="primary" onClick={(() => setLocale(locales.ES))}>
+          Set to Spanish (es_ES)
+        </Button>
+        <Button type="secondary" onClick={(() => setLocale(locales.CA))}>
+          Set to Catalan (ca_ES)
+        </Button>
+      </Form>
+    </ConfigProvider>
   );
-}
+};
 
 export default App;
